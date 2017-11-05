79c2de75a1b16b82c2b655c6465c48afaf83cdf1
Colin Hebert <hebert.colin@gmail.com>
2017-11-05 13:52:08 +1100

    Handle pom generation in kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,10 +52,70 @@ tasks {
         })
     }
 
-    create("dokka") {
+    create("ossrhAuthentication") {
+        if (!(hasProperty("ossrh.username") && hasProperty("ossrh.password"))) {
+            throw InvalidUserDataException("no OSSRH username and/or password!")
+        }
+    }
 
+    "uploadArchives"(Upload::class) {
+        dependsOn("ossrhAuthentication")
+
+        repositories {
+            withConvention(MavenRepositoryHandlerConvention::class) {
+                mavenDeployer {
+                    beforeDeployment { signing.signPom(this) }
+
+                    withGroovyBuilder {
+                        "repository"("url" to uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")) {
+                            "authentication"("userName" to properties["ossrh.username"], "password" to properties["ossrh.password"])
+                        }
+                        "snapshotRepository"("url" to uri("https://oss.sonatype.org/content/repositories/snapshots/")) {
+                            "authentication"("userName" to properties["ossrh.username"], "password" to properties["ossrh.password"])
+                        }
+                    }
+
+                    pom.project {
+                        withGroovyBuilder {
+                            "name"("Konfig")
+                            "packaging"("jar")
+                            "description"("Konfiguration for Cotlin... no, Configuration for Kotlin")
+                            "url"("https://github.com/npryce/konfig")
+
+                            "scm" {
+                                "connection"( "git@github.com:npryce/konfig.git")
+                                "url"("https://github.com/npryce/konfig")
+                            }
+
+                            "licenses" {
+                                "license" {
+                                    "name"("Apache 2.0")
+                                    "url"("http://opensource.org/licenses/Apache-2.0")
+                                }
+                            }
+
+                            "developers" {
+                                "developer" {
+                                    "id"("npryce")
+                                    "name"("Nat Pryce")
+                                }
+                                "developer" {
+                                    "id"("dmcg")
+                                    "name"("Duncan McGregor")
+                                }
+                            }
+                        }
+                    }
+
+                    pom { withGroovyBuilder { } }
+                }
+            }
+        }
+    }
+
+    create("dokka") {
     }
 }
 
 fun <T : Any, U : Any> Any.closureOf(action: (T) -> U) = KotlinClosure1(action, this, this)
-fun <T : Any, U : Any, V: Any> Any.closureOf(action: (T, U) -> V) = KotlinClosure2(action, this, this)
+fun <T : Any, U : Any, V : Any> Any.closureOf(action: (T, U) -> V) = KotlinClosure2(action, this, this)
